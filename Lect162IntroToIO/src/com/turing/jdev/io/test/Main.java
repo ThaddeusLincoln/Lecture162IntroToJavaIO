2090779b14e1f0a365281c4da537cd311d84508f
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-29 00:01:26 +0200

    By just throwing the Exception we can make the code simpler, and we won't be able to use the catch block.

--- Lect162IntroToIO/src/com/turing/jdev/io/test/Main.java
+++ Lect162IntroToIO/src/com/turing/jdev/io/test/Main.java
@@ -5,25 +5,17 @@ import java.io.IOException;
 
 public class Main {
 
-	 public static void main(String[] args){
+	 public static void main(String[] args) throws IOException{
 		 
 		 FileWriter locFile = null;
 		 try {
-			locFile = new FileWriter("locations.txt");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
-			try {
-				
-				// this line will guarantee that we don't get a NPE in case
-				// locFile = new FileWriter("locations.txt");
-				// doesn't executes correctly, for whatever reason and locFile is still null when whis line
-				// it attempted to be executed.
-				if(locFile != null){
-					locFile.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
+		
+			 locFile = new FileWriter("locations.txt");
+		
+		 }finally{
+
+			if(locFile != null){
+				locFile.close();
 			}
 		}
 	 }
